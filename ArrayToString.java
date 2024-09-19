3ff939357a7eeb2a65904bfc54d645b5d3551082
Abhinay-Sajwan007 <abhinay.sajwan@gmail.com>
2024-09-19 00:04:58 -0700

    Update ArrayToString.java

--- ArrayToString.java
+++ ArrayToString.java
@@ -123,5 +123,21 @@ class ArrayToString{
 			}
 			System.out.println("");
 		}
-	}	
+	}
+
+	public static void selectionSort(){
+		int arr[] = {64,25,12,14,13,-1};
+		for(int i=0;i<arr.length-1;i++){
+		    int index=i;
+		    for (int j=i+1;j<=arr.length-1;j++){
+		        if(arr[index] > arr[j]){
+		            index=j;
+		        }
+		    }
+		    //swap
+		    arr[index] +=arr[i] - (arr[i] = arr[index]);
+		    System.out.println("Step "+ (i+1)+" : " +Arrays.toString(arr));
+		}
+		System.out.println(Arrays.toString(arr));
+	}
 }
